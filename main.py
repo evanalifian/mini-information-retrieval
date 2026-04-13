c764290d52bf7c7fd1f6e5f37a5aed8f5b726e5a
evanalifian <evanalifian@gmail.com>
2026-04-13 14:42:03 +0700

    refactor: implements text preprocessing and refactor the code to oop

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from read_documents import documents as docs
+from text_preprocessing import TextPreprocessing
 
 # stopwords list
 stopwords = ["dengan", "dan", "untuk", "pada", "di", "dari", "yang", "karena", "akibat"]
-
 # query
 query = "Pencegahan demam tinggi"
+
+
+# >= step 1: text preprocessing
+tp = TextPreprocessing(stopwords, query, docs)
+# tokenization
+tp.tokenization()
+# case folding
+tp.case_folding()
+# stopword removal
+tp.stopword_removal()
+# stemming
+tp.stemming()
+# get tokens
+tokens = tp.tokens
+
+
+# >= step 2: get documents length and create inverted index
+q = tp.get_doc_length(tokens["q"])
+d4 = tp.get_doc_length(tokens["d4"])
+d5 = tp.get_doc_length(tokens["d5"])
+d6 = tp.get_doc_length(tokens["d6"])
+d7 = tp.get_doc_length(tokens["d7"])
+d8 = tp.get_doc_length(tokens["d8"])
