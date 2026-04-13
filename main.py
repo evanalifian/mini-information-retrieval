d3a6357147f9377532c91759b2ad4876ae6dbc17
evanalifian <evanalifian@gmail.com>
2026-04-13 10:53:19 +0700

    feat: implements Sastrawi Python

---
+++ main.py
@@ -0,0 +1,7 @@
+from read_documents import documents as docs
+
+# stopwords list
+stopwords = ["dengan", "dan", "untuk", "pada", "di", "dari", "yang", "karena", "akibat"]
+
+# query
+query = "Pencegahan demam tinggi"
