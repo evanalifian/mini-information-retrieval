43d52f8a70978e8929a4d3796ed37cd281c42f81
evanalifian <evanalifian@gmail.com>
2026-04-16 17:08:34 +0700

    feat: crete corpus table

--- main.py
+++ main.py
@@ -28,3 +28,39 @@ d5 = tp.get_doc_length(tokens["d5"])
 d6 = tp.get_doc_length(tokens["d6"])
 d7 = tp.get_doc_length(tokens["d7"])
 d8 = tp.get_doc_length(tokens["d8"])
+total_docs = list(tokens.values())
+avgl = (d4 + d5 + d6 + d7 + d8) / len(total_docs[1:]) #avgl value
+
+
+# >= step 3: corpus
+# only document (without query)
+doc_tokens = {k: v for k, v in tokens.items() if k != "q"}
+
+# take all unique terms
+corpus = set()
+for t in doc_tokens.values():
+    corpus.update(t)
+
+corpus = sorted(corpus)
+
+
+# create corpus table
+corpus_table = {}
+
+for term in corpus:
+    corpus_table[term] = {}
+    df = 0
+
+    for doc, words in doc_tokens.items():
+        tf = words.count(term)
+        corpus_table[term][doc] = tf
+
+        if tf > 0:
+            df += 1
+
+    corpus_table[term]["dft"] = df
+
+for k, v in corpus_table.items():
+    print(k)
+    print(v)
+    print()
