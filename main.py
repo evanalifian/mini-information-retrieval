1df567aeaed2809ac5b411d366b19752e386e87f
evanalifian <evanalifian@gmail.com>
2026-04-17 17:40:40 +0700

    docs: adding comment

--- main.py
+++ main.py
@@ -29,16 +29,19 @@ d5 = tp.get_doc_length(tokens["d5"])
 d6 = tp.get_doc_length(tokens["d6"])
 d7 = tp.get_doc_length(tokens["d7"])
 d8 = tp.get_doc_length(tokens["d8"])
+# get all docs
 total_docs = list(tokens.values())
+# calulate avgl
 avgl = (d4 + d5 + d6 + d7 + d8) / len(total_docs[1:]) #avgl value
 
 
-# >= step 3: corpus
+# >= step 3: corpus table
 corpus = InvertedIndex(tokens)
-
+# select all unique terms from each docs
 corpus.take_unique_term()
+# create corpus table
 corpus.create_corpus_table()
-
+# get corpus taable
 corpus_table = corpus.corpus_table
 
 for term, doc in corpus_table.items():
