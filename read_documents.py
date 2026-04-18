8869a83ae43afe4e71fdf77ffa378884b4afb22b
evanalifian <evanalifian@gmail.com>
2026-04-19 00:33:38 +0700

    feat: read all text documents

--- read_documents.py
+++ read_documents.py
@@ -2,6 +2,6 @@
 documents = {}
 
 # read each file document an store to dict
-for i in range(4, 8 + 1):
+for i in range(1, 10 + 1):
     with open(f"./documents/document-{i}.txt") as f:
         documents[f"d{i}"] = f.read()
